2e1067f067a387e2e6637071d3d0b23f431c0134
David Kierzkowski <kd8eyf@digitalham.info>
2012-08-28 02:53:27 -0300

    Update scripts/DmrMarcRip.php
    fixed up some comments add license

--- scripts/DmrMarcRip.php
+++ scripts/DmrMarcRip.php
@@ -17,8 +17,8 @@
 //You should have received a copy of the GNU General Public License
 //along with this program; if not, write to the Free Software
 //Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-////////////////////////////////////////CHANGE THESE///////////////////////////////////////////////////////////
-include '/home/kd8eyf/include/dmrdbwrite.inc';                                     //init database                              
+//----------------------------------CHANGE THESE--------------------------------------------------------------//
+include '/home/kd8eyf/include/dmrdbwrite.inc';                             //Add this file for database creds.                              
 $UserUrl = "http://www.n6dva.org/trbo-database/trbo_users_view.php?SearchString=&Print.x=67&Print.y=3&nav_menu=%23&SortField=&SelectedID=&SortDirection=&FirstRecord=1";
 $RepeaterUrl = "http://www.n6dva.org/trbo-database/trbo_repeaters_view.php?SearchString=&Print.x=67&Print.y=3&nav_menu=%23&SortField=&SelectedID=&SortDirection=&FirstRecord=1";
 $PrefixUrl = "http://www.n6dva.org/trbo-database/trbo_radio_id_scheme_view.php?SearchString=&Print.x=67&Print.y=3&nav_menu=%23&SortField=&SelectedID=&SortDirection=&FirstRecord=1";
@@ -27,85 +27,59 @@ $RepeaterFieldNames =   array('DmrID','Callsign','City','State','Country','Frequ
 $UserFieldNames =       array('DmrID','Callsign','Name','City','State','Country','Radio','HomeRepeaterID','Remarks');
 $PrefixFieldNames =     array('DmrID','Country','State','Group');
 $TalkgroupFieldNames =  array('Network','Region','DmrID','Assignment','Notes');
-////////////////////////////////////////DONT CHANGE THESE//////////////////////////////////////////////////////
-libxml_use_internal_errors(true);                                           //DONT WARN ABOUT DOM PARSING
-date_default_timezone_set('UTC');                                           //SET TIMEZONE TO UTC
-$DateTime = date('Y-m-d H:i:s',time());                                     //SET DATETIME STAMP TO SQL COMPATABILE 
-$hostname = exec("hostname -f");                                            //GET SYSTEM HOSTNAME
+//----------------------------------DONT CHANGE THESE---------------------------------------------------------//
+
+libxml_use_internal_errors(true);                                          //DONT WARN ABOUT DOM PARSING
+date_default_timezone_set('UTC');                                          //SET TIMEZONE TO UTC
+$DateTime = date('Y-m-d H:i:s',time());                                    //SET DATETIME STAMP TO SQL COMPATABILE 
+$hostname = exec("hostname -f");                                           //GET SYSTEM HOSTNAME
 
 $OldUser = Array();
 $OldPrefix = Array();
 $OldRepeater = Array();       
 $OldTalkgroup = Array();                                                        
 
-$NewUser = RipData($UserUrl,$UserFieldNames,"0");                               //RIP SUBSCRIBER DATA
-$NewRepeater = RipData($RepeaterUrl,$RepeaterFieldNames,"0");                   //RIP REPEATER DATA
-$NewPrefix = RipData($PrefixUrl,$PrefixFieldNames,"0");                         //RIP PREFIX DATA
+$NewUser = RipData($UserUrl,$UserFieldNames,"0");                          //RIP SUBSCRIBER DATA
+$NewRepeater = RipData($RepeaterUrl,$RepeaterFieldNames,"0");              //RIP REPEATER DATA
+$NewPrefix = RipData($PrefixUrl,$PrefixFieldNames,"0");                    //RIP PREFIX DATA
 $NewTalkgroup = RipData($TalkgroupUrl,$TalkgroupFieldNames,"2");
-$OldUser = GetOldData("User",$UserFieldNames);                              //GET CURRENT SUBSCRIBERS
-$OldRepeater = GetOldData("Repeater",$RepeaterFieldNames);                  //GET CURRENT REPEATERS
-$OldPrefix = GetOldData("Prefix",$PrefixFieldNames);                        //GET CURRENT PREFIXE
+$OldUser = GetOldData("User",$UserFieldNames);                             //GET CURRENT SUBSCRIBERS
+$OldRepeater = GetOldData("Repeater",$RepeaterFieldNames);                 //GET CURRENT REPEATERS
+$OldPrefix = GetOldData("Prefix",$PrefixFieldNames);                       //GET CURRENT PREFIXE
 $OldTalkgroup = GetOldData("Talkgroup",$TalkgroupFieldNames);        
 
-$AddUser =      array_diff_key($NewUser,$OldUser);                               //CALCULATE WHAT USERS ARE NEW
-$AddRepeater =  array_diff_key($NewRepeater,$OldRepeater);                   //CALCULATE WHAT REPEATERS ARE NEW
-$AddPrefix =    array_diff_key($NewPrefix,$OldPrefix);                         //CALCULATE WHAT PREFIXS ARE NEW
+$AddUser =      array_diff_key($NewUser,$OldUser);                         //CALCULATE WHAT USERS ARE NEW
+$AddRepeater =  array_diff_key($NewRepeater,$OldRepeater);                 //CALCULATE WHAT REPEATERS ARE NEW
+$AddPrefix =    array_diff_key($NewPrefix,$OldPrefix);                     //CALCULATE WHAT PREFIXS ARE NEW
 $AddTalkgroup = array_diff_key($NewTalkgroup,$OldTalkgroup);     
 
-$DelUser =      array_diff_key($OldUser,$NewUser);                               //CALCULATE WHAT USERS ARE REMOVED
-$DelRepeater =  array_diff_key($OldRepeater,$NewRepeater);                   //CALCULATE WHAT REPEATERS WHERE REMOVED
-$DelPrefix =    array_diff_key($OldPrefix,$NewPrefix);                         //CALCULATE WHAT PREFIXS ARE REMOVED
+$DelUser =      array_diff_key($OldUser,$NewUser);                         //CALCULATE WHAT USERS ARE REMOVED
+$DelRepeater =  array_diff_key($OldRepeater,$NewRepeater);                 //CALCULATE WHAT REPEATERS WHERE REMOVED
+$DelPrefix =    array_diff_key($OldPrefix,$NewPrefix);                     //CALCULATE WHAT PREFIXS ARE REMOVED
 $DelTalkgroup = array_diff_key($OldTalkgroup,$NewTalkgroup);  
 
-$BothUser =     array_intersect_key($NewUser,$OldUser);                         //CALCULATE WHAT USERS EXISIT IN BOTH
-$BothRepeater = array_intersect_key($NewRepeater,$OldRepeater);             //CALCULATE WHAT REPEATERS EXISIT IN BOTH
-$BothPrefix =   array_intersect_key($NewPrefix,$OldPrefix);                   //CALCULATE WHAY PREFIXS EXISIT IN BOTH
+$BothUser =     array_intersect_key($NewUser,$OldUser);                    //CALCULATE WHAT USERS EXISIT IN BOTH
+$BothRepeater = array_intersect_key($NewRepeater,$OldRepeater);            //CALCULATE WHAT REPEATERS EXISIT IN BOTH
+$BothPrefix =   array_intersect_key($NewPrefix,$OldPrefix);                //CALCULATE WHAY PREFIXS EXISIT IN BOTH
 $BothTalkgroup =array_intersect_key($NewTalkgroup,$OldTalkgroup);
 
 
-DBAdd($AddUser,"User");                                                     //INSERT ADDITIONS TO SUBSCRIBER TABLE
-DBAdd($AddRepeater,"Repeater");                                             //INSERT ADDITIONS TO REPEATERS TABLE    
-DBAdd($AddPrefix,"Prefix");                                                 //INSERT ADDITIONS TO PREFIXES TABLE
-DBAdd($AddTalkgroup,"Talkgroup");                                                 //INSERT ADDITIONS TO PREFIXES TABLE
+DBAdd($AddUser,"User");                                                    //INSERT ADDITIONS TO SUBSCRIBER TABLE
+DBAdd($AddRepeater,"Repeater");                                            //INSERT ADDITIONS TO REPEATERS TABLE    
+DBAdd($AddPrefix,"Prefix");                                                //INSERT ADDITIONS TO PREFIXES TABLE
+DBAdd($AddTalkgroup,"Talkgroup");                                          //INSERT ADDITIONS TO PREFIXES TABLE
 
-DBChange($OldUser,$NewUser,$BothUser,"User");                               //MAKE CHANGED TO SUBSRIBER TABLE
-DBChange($OldRepeater,$NewRepeater,$BothRepeater,"Repeater");               //MAKE CHANGES TO REPETAER TABLE
-DBChange($OldPrefix,$NewPrefix,$BothPrefix,"Prefix");                       //MAKE CHANGES TO PREFIX TABLE
+DBChange($OldUser,$NewUser,$BothUser,"User");                              //MAKE CHANGED TO SUBSRIBER TABLE
+DBChange($OldRepeater,$NewRepeater,$BothRepeater,"Repeater");              //MAKE CHANGES TO REPETAER TABLE
+DBChange($OldPrefix,$NewPrefix,$BothPrefix,"Prefix");                      //MAKE CHANGES TO PREFIX TABLE
 DBChange($OldTalkgroup,$NewTalkgroup,$BothTalkgroup,"Talkgroup");
-DBRemove($DelUser,"User");                                                  //REMOVE DELETED SUBSCRIBERS FROM TABLE
-DBRemove($DelRepeater,"Repeater");                                          //REMOVE DELETED REPEATERS FROM TABLE
-DBRemove($DelPrefix,"Prefix");                                              //REMOVE DELETED PREFIXES FROM TABLE
+DBRemove($DelUser,"User");                                                 //REMOVE DELETED SUBSCRIBERS FROM TABLE
+DBRemove($DelRepeater,"Repeater");                                         //REMOVE DELETED REPEATERS FROM TABLE
+DBRemove($DelPrefix,"Prefix");                                             //REMOVE DELETED PREFIXES FROM TABLE
 DBRemove($DelTalkgroup,"Talkgroup"); 
 
 
 print "DAILY DMR-MARC DB UPDATE ON: ".$hostname."\n";
-print "STARTED ON: ".$DateTime."\n";
-if(count($OldUser)){print "SUBSCRIBER RECORDS ALREADING IN DB: ".count($OldUser)."\n";}
-if(count($NewUser)){print "SUBSCRIBER RECORDS RIPPED: ".count($NewUser)."\n";}
-if(count($DelUser)){print "SUBSCRIBER RECORDS REMOVED FROM RIP: ".count($DelUser)."\n";}
-if(count($AddUser)){print "SUBSCRIBER RECORDS ADDED TO DB: ".count($AddUser)."\n";}
-if(count($BothUser)){print "SUBSCRIBER RECORDS UNCHANGED: ".count($BothUser)."\n";}
-if(count($DiffUser)){print "SUBSCRIBER RECORDS ALREADY EXIST THAT HAVE CHANGED: ".count($DiffUser)."\n";}
-if(count($OldRepeater)){print "REPEATER RECORDS ALREADY IN DB: ".count($OldRepeater)."\n";}
-if(count($NewRepeater)){print "REPEATER RECORDS RIPPED: ".count($NewRepeater)."\n";}
-if(count($DelRepeater)){print "REPEATER RECORDS REMOVED FROM RIP: ".count($DelRepeater)."\n";}
-if(count($AddRepeater)){print "REPETAER RECORDS ADDED TO DB: ".count($AddRepeater)."\n";}
-if(count($BothRepeater)){print "REPEATER RECORDS UNCHANGED: ".count($BothRepeater)."\n";}
-if(count($DiffTalkgroup)){print "Talkgroup RECORDS ALREADY EXIST THAT HAVE CHANGED: ".count($DiffTalkgroup)."\n";}
-if(count($OldTalkgroup)){print "Talkgroup RECORDS ALREADY IN DB: ".count($OldTalkgroup)."\n";}
-if(count($NewTalkgroup)){print "Talkgroup RECORDS RIPPED: ".count($NewTalkgroup)."\n";}
-if(count($DelTalkgroup)){print "Talkgroup RECORDS REMOVED FROM RIP: ".count($DelTalkgroup)."\n";}
-if(count($AddTalkgroup)){print "Talkgroup RECORDS ADDED TO DB: ".count($AddTalkgroup)."\n";}
-if(count($BothTalkgroup)){print "Talkgroup RECORDS UNCHANGED: ".count($BothTalkgroup)."\n";}
-if(count($DiffTalkgroup)){print "Talkgroup RECORDS ALREADY EXIST THAT HAVE CHANGED: ".count($DiffTalkgroup)."\n";}
-if(count($OldPrefix)){print "Prefix RECORDS ALREADY IN DB: ".count($OldPrefix)."\n";}
-if(count($NewPrefix)){print "Prefix RECORDS RIPPED: ".count($NewPrefix)."\n";}
-if(count($DelPrefix)){print "Prefix RECORDS REMOVED FROM RIP: ".count($DelPrefix)."\n";}
-if(count($AddPrefix)){print "Prefix RECORDS ADDED TO DB: ".count($AddPrefix)."\n";}
-if(count($BothPrefix)){print "Prefix RECORDS UNCHANGED: ".count($BothPrefix)."\n";}
-if(count($DiffPrefix)){print "Prefix RECORDS ALREADY EXIST THAT HAVE CHANGED: ".count($DiffPrefix)."\n";}
-print "DMR-MARC RIP DONE\n";
-
 
 function BuildSqlRemove($Array,$Table,$Where) {
      $Query = "DELETE FROM `".$Table."` WHERE ".$Where;
@@ -127,7 +101,7 @@ function BuildSqlUpdate($Array,$Table,$Where) {
      $Query = "UPDATE `".$Table."` SET ".$Query." WHERE `".$Table."`.".$Where;
      return ($Query);
 }
-function LogChange($NewItem,$ChangeType, $RecordType) //logs to the change log
+function LogChange($NewItem,$ChangeType, $RecordType)                      //LOG CHANGES TO THE CHANGE LOG
 {
     global $DateTime;
      switch ($ChangeType) {
@@ -154,15 +128,15 @@ function RipData($Url,$ColumnNames,$key) {
      $Dom = new DOMDocument();
      $doctype = DOMImplementation::createDocumentType("html","-//W3C//DTD XHTML 1.1//EN","http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd");
      $Dom->appendChild($doctype);
-     $Page = $Dom->loadHTMLFile($Url); // new dom object for users
-     $Dom->preserveWhiteSpace = false; //discard white space
-     $Table = $Dom->getElementsByTagName('table'); //the table by its tag name
-     $Rows = $Table->item(0)->getElementsByTagName('tr'); //get all rows from the table
-      foreach ($Rows as $row) { // loop over the table rows
-          $cols = $row->getElementsByTagName('td'); // get each column by tag name
-          if (is_numeric($cols->item($key)->textContent)) {
+     $Page = $Dom->loadHTMLFile($Url);                                     //CRAEATE NEW DOM OBJECT
+     $Dom->preserveWhiteSpace = false;                                     //DELETE HTML WHITE SPACES
+     $Table = $Dom->getElementsByTagName('table');                         //IDENTIFY START OF TABLE
+     $Rows = $Table->item(0)->getElementsByTagName('tr');                  //IDENTIFY ROWS
+      foreach ($Rows as $row) {                                            //START A LOOP FOR EACH ROW
+          $cols = $row->getElementsByTagName('td');                        //GET THE COLUMNS
+          if (is_numeric($cols->item($key)->textContent)) {                //IS THIS AN ITEM THAT WE WANT TO PROCESS
                $i = 0;
-               foreach ($cols as $col) {
+               foreach ($cols as $col) {                                   //LOOP THROUGH EACH COL AND STORE IN ARRAY
                     $array[$cols->item($key)->nodeValue][$ColumnNames[$i]] =str_replace("NBSP","",strtoupper(trim(preg_replace("/[^0-9a-zA-Z .-]/","",$col->textContent))));
                     $i++;
                }
@@ -170,14 +144,14 @@ function RipData($Url,$ColumnNames,$key) {
      }
      return $array;
 }
-function GetOldData($TableName,$FieldNames) {    
+function GetOldData($TableName,$FieldNames) {                              //GET DATA FROM YESTERDAYS DMR-MARC DATABASE
      $Query = "SELECT * FROM `".$TableName."`;";
      $Result = mysql_query($Query) or die("Error in query: $query ".mysql_error());
      $NumResults = mysql_num_rows($Result);
      if ($NumResults > 0) {
           while ($row = mysql_fetch_array($Result,MYSQL_ASSOC)) {
                foreach ($FieldNames as $FieldName) {
-                    $array[$row[DmrID]][$FieldName] = $row[$FieldName]; //AM I OVERTHINKING THIS?
+                    $array[$row[DmrID]][$FieldName] = $row[$FieldName];     //AM I OVERTHINKING THIS?
                }
           }
      }
@@ -191,7 +165,7 @@ function GetOldData($TableName,$FieldNames) {
      }
 }
 
-function DBChange($Old,$New,$Both,$Table){
+function DBChange($Old,$New,$Both,$Table){                                 //CHANGE AN ITEM IN THE DATABASE
      foreach ($Both as $DiffRow) {
           $Diff = array_diff($New[$DiffRow[DmrID]],$Old[$DiffRow[DmrID]]);
           if ($Diff) {
@@ -206,7 +180,7 @@ function DBChange($Old,$New,$Both,$Table){
           }
      }
 }
-function DBAdd($new,$table) {
+function DBAdd($new,$table) {                                              //ADD A ITEM TO THE DATABASE
      if (count($new) != false) {
           foreach ($new as $AddRow) {
                $Query = BuildSqlInsert($AddRow,$table)."\n";
@@ -215,7 +189,7 @@ function DBAdd($new,$table) {
           }
      }
 }
-function DBRemove($del,$table) {
+function DBRemove($del,$table) {                                           //REMOVE A ITEM FROM TEH DATABASE
      if (count($del) != false) {
           foreach ($del as $DelRow) {
                $Where = "`DmrID`='".$DelRow[DmrID]."'";
