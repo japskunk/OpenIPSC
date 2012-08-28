d119f3b56ddcd7ea5a7360aa10a6f5299050b833
KD8EYF <KD8EYF@digitalham.info>
2012-08-28 14:00:55 -0400

    added some  verbose info / process header offsets / start processing dmr field

--- dmrmon-remote/dmrmon-remote.c
+++ dmrmon-remote/dmrmon-remote.c
@@ -25,19 +25,46 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA. 
 #include<netinet/udp.h>
 #include<netinet/ip.h>
 #include<getopt.h>
+struct UDP_hdr {
+	u_short	uh_sport;		/* source port */
+	u_short	uh_dport;		/* destination port */
+	u_short	uh_ulen;		/* datagram length */
+	u_short	uh_sum;			/* datagram checksum */
+};
 int  debug = 0;
 char *devname = NULL;
 
 void usage( int8_t e );
 void processPacket(u_char *arg, const struct pcap_pkthdr* pkthdr, const u_char * packet)
 {
+	struct ip * ip;
+	struct UDP_hdr * udp;
+	unsigned int IP_header_length;	
+	unsigned int capture_len = pkthdr->len;
+	unsigned int SourceID;
+	unsigned int PacketType;
+	unsigned int UserID;
 	int i=0, *counter = (int *)arg;
+	
+	packet += sizeof (struct ether_header);
+	capture_len -= sizeof(struct ether_header);
+	ip = (struct ip*) packet;
+	IP_header_length = ip->ip_hl *4;
+	packet += IP_header_length;
+	capture_len -= IP_header_length;	
+	udp = (struct UDP_hdr*) packet;
+	packet += sizeof (struct UDP_hdr);
+	if (	
 	if (debug == 1){
-		printf("Packet Count: %d\n", ++(*counter));
-	        printf("Received Packet Size: %d\n", pkthdr->len);
+		printf("Packet Count:\t%d\n", ++(*counter));	
+		printf("Packet Size:\t%d\n", capture_len);
+		printf("Size ETH Header\t%lu\n",sizeof(struct ether_header));
+		printf("Size IP Header\t%u\n",IP_header_length);
+		printf("UDP Src Port\t%d\n", ntohs(udp->uh_sport));
+		printf("IDP Dst Port\t%d\n", ntohs(udp->uh_dport));
 	}
-        for (i=0; i<pkthdr->len; i++) {
-	        printf("%x", packet[i]);
+        for (i=0; i<capture_len; i++) {
+	        printf("%x ", packet[i]);
 	
 }                        
 printf("\n");
@@ -78,12 +105,12 @@ int main(int argc, char *argv[] )
         char errbuf[100] , devs[100][100];
         int count = 1 , n;
         handle = pcap_open_live(devname , 65536 , 1 , 0 , errbuf);
-
         if (handle == NULL) {
                 fprintf(stderr, "Couldn't open device %s : %s\n" , devname , errbuf);
                 exit(1);
         }
         pcap_compile(handle, &fcode, packet_filter, 1, netmask);
+
 	if ( pcap_loop(handle, -1, processPacket, (u_char *)&count) == -1) {
                 fprintf(stderr, "ERROR: %s\n", pcap_geterr(descr) );
                 exit(1);
@@ -94,13 +121,21 @@ int main(int argc, char *argv[] )
 }
 void usage(int8_t e)
 {
-        printf("dmrmon - The openIPSC DMR network monitor client\n"
-               "         Used in congution with dmrserv\n"
-	       "usage: dmrmon -i interface \n"
-               "              -h is this help\n"
-               "              -V version information\n"
-               "	      -d debug information\n"
-		"");
+        printf(	"Usage: dmrmon-remotei [OPTION]... [REMOTE SERVER]...\n"
+		"Listen send DMR data for remote server for processing\n" 
+		"\n"
+		"   -i, --interface	Interface to listen on\n"
+		"   -h, --help		This Help\n"
+		"   -V, --version	Version Information\n"
+		"   -d, --debug		Show verbose information\n"
+		"\n"
+		"With no REMOTE SERVER or REMOTE SERVER is -, output to standard output\n"
+		"\n"
+		"Examples:\n"
+		"   dmrmon-remote -i eth0 192.168.10.20 50000\n"	
+		"   Send the DMR data heard on eth0 to remote server\n"      
+		"\n"
+		"Report cat bugs to kd8eyf@digitalham.info\n");
         exit(e);
 }
 
